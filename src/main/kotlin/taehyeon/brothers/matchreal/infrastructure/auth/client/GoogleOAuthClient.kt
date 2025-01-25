059c21c247557739b075eb17c59256c8b6d66651
Jayon <basejin0810@naver.com>
2025-01-25 15:10:27 +0900

    refactor: 디코드 설정

--- src/main/kotlin/taehyeon/brothers/matchreal/infrastructure/auth/client/GoogleOAuthClient.kt
+++ src/main/kotlin/taehyeon/brothers/matchreal/infrastructure/auth/client/GoogleOAuthClient.kt
@@ -6,10 +6,13 @@ import okhttp3.OkHttpClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import taehyeon.brothers.matchreal.domain.user.User
-import taehyeon.brothers.matchreal.infrastructure.common.HttpClient
+import taehyeon.brothers.matchreal.exception.OAuthException
 import taehyeon.brothers.matchreal.infrastructure.auth.client.dto.GoogleTokenResponse
 import taehyeon.brothers.matchreal.infrastructure.auth.client.dto.GoogleUserResponse
-import taehyeon.brothers.matchreal.exception.OAuthException
+import taehyeon.brothers.matchreal.infrastructure.common.HttpClient
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
+
 
 @Component
 class GoogleOAuthClient(
@@ -22,9 +25,11 @@ class GoogleOAuthClient(
 ) : HttpClient(okHttpClient, objectMapper) {
 
     fun generateAccessToken(authorizationCode: String, redirectUri: String): String {
+        val decodedCode: String = URLDecoder.decode(authorizationCode, StandardCharsets.UTF_8)
+
         val formBody = FormBody.Builder()
             .add("grant_type", "authorization_code")
-            .add("code", authorizationCode)
+            .add("code", decodedCode)
             .add("client_id", clientId)
             .add("client_secret", clientSecret)
             .add("redirect_uri", redirectUri)
