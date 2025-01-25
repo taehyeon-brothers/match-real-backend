44044984f86cceba0423c214fa23d45c42d0aa8d
Jayon <basejin0810@naver.com>
2025-01-25 15:00:21 +0900

    submodule 최신화

--- src/main/kotlin/taehyeon/brothers/matchreal/infrastructure/auth/client/OAuthClient.kt
+++
@@ -1,8 +0,0 @@
-package taehyeon.brothers.matchreal.infrastructure.auth.client
-
-import taehyeon.brothers.matchreal.domain.user.User
-
-interface OAuthClient {
-    fun generateAccessToken(authorizationCode: String, redirectUri: String): String
-    fun getUserInfo(accessToken: String): User
-} 
