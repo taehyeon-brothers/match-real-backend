dfe4a6b5e3cffe7e81906486fc35ba00004e8dbe
pjy1368 <basejin0810@naver.com>
2025-01-28 19:32:31 +0900

    refactor: swagger operation 추가

--- src/main/kotlin/taehyeon/brothers/matchreal/presentation/auth/controller/AuthController.kt
+++ src/main/kotlin/taehyeon/brothers/matchreal/presentation/auth/controller/AuthController.kt
@@ -1,5 +1,7 @@
 package taehyeon.brothers.matchreal.presentation.auth.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import taehyeon.brothers.matchreal.application.auth.service.AuthService
@@ -9,11 +11,13 @@ import taehyeon.brothers.matchreal.presentation.auth.dto.request.RefreshTokenReq
 import taehyeon.brothers.matchreal.presentation.auth.dto.response.TokenResponse
 import taehyeon.brothers.matchreal.presentation.argumentresolver.RequiredLogin
 
+@Tag(name = "인증", description = "인증 관련 API")
 @RestController
 @RequestMapping("/api/v1")
 class AuthController(
     private val authService: AuthService
 ) {
+    @Operation(summary = "구글 로그인")
     @PostMapping("/login/google")
     fun login(
         @RequestBody request: AuthorizationCodeRequest
@@ -21,6 +25,7 @@ class AuthController(
         return ResponseEntity.ok(authService.googleLogin(request))
     }
 
+    @Operation(summary = "토큰 갱신")
     @PostMapping("/login/refresh")
     fun refreshToken(
         @RequestBody request: RefreshTokenRequest
@@ -29,6 +34,7 @@ class AuthController(
         return ResponseEntity.ok(tokenResponse)
     }
 
+    @Operation(summary = "로그아웃")
     @DeleteMapping("/logout")
     fun logout(@RequiredLogin user: User): ResponseEntity<Unit> {
         authService.logout(user)
