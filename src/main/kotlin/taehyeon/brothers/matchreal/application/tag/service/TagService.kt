ffe32c19f84950dae60a22d44bde5e44fbf4db37
김태현 <kth990303@woowahan.com>
2025-02-16 14:29:34 +0900

    feat: 태그를 유저가 추가 및 삭제하는 비즈니스 로직 작성

--- src/main/kotlin/taehyeon/brothers/matchreal/application/tag/service/TagService.kt
+++ src/main/kotlin/taehyeon/brothers/matchreal/application/tag/service/TagService.kt
@@ -1,14 +1,18 @@
 package taehyeon.brothers.matchreal.application.tag.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
 import taehyeon.brothers.matchreal.domain.daily.Daily
 import taehyeon.brothers.matchreal.domain.tag.Tag
+import taehyeon.brothers.matchreal.exception.database.EntityNotFoundException
+import taehyeon.brothers.matchreal.infrastructure.daily.repository.DailyRepository
 import taehyeon.brothers.matchreal.infrastructure.tag.repository.TagRepository
 
 @Service
 class TagService(
     private val tagRepository: TagRepository,
+    private val dailyRepository: DailyRepository,
 ) {
 
     fun addTagsByDailyImage(daily: Daily, dailyImage: MultipartFile): List<Tag> {
@@ -24,5 +28,21 @@ class TagService(
             Tag.createForm(daily, "데이트"),
         )
         return tagRepository.saveAll(tags)
+    }
+
+    @Transactional
+    fun addTagByUser(dailyId: Long, tagName: String): Long {
+        val daily = dailyRepository.findById(dailyId)
+            .orElseThrow { EntityNotFoundException(message = "데일리가 존재하지 않습니다. dailyId: $dailyId") }
+        val tag = Tag.createForm(daily, tagName)
+        val savedTag = tagRepository.save(tag)
+        return savedTag.id
+    }
+
+    @Transactional
+    fun removeTagByUser(tagId: Long) {
+        tagRepository.findById(tagId)
+            .orElseThrow { EntityNotFoundException(message = "태그가 존재하지 않습니다. tagId: $tagId") }
+        tagRepository.deleteById(tagId)
     }
 }
