54e9ede680b1f83a64506f7906dd286822ad4200
Jayon <basejin0810@naver.com>
2025-02-22 10:51:17 +0900

    refactor: argument resolver 무시

--- src/main/kotlin/taehyeon/brothers/matchreal/presentation/config/SwaggerConfig.kt
+++ src/main/kotlin/taehyeon/brothers/matchreal/presentation/config/SwaggerConfig.kt
@@ -2,11 +2,17 @@ package taehyeon.brothers.matchreal.presentation.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.utils.SpringDocUtils
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import taehyeon.brothers.matchreal.presentation.argumentresolver.RequiredLogin
 
 @Configuration
 class SwaggerConfig {
+
+    init {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(RequiredLogin::class.java)
+    }
 
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
